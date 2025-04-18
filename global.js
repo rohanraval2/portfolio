8cdd8e6485a3c7b051bcbf22bee4aec6b42621ff
rohanraval2 <70520403+rohanraval2@users.noreply.github.com>
2025-04-18 12:32:05 -0700

    dark mode enabled update lab03

--- global.js
+++ global.js
@@ -3,43 +3,64 @@ console.log("IT'S ALIVE!")
 const $$ = (selector) => Array.from(document.querySelectorAll(selector));
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                     // Local development
-  : "/portfolio/";     // GitHub Pages repo name — CHANGE THIS!
+  ? "/"                     
+  : "/portfolio/";     
 
-// Pages to appear in the nav
 let pages = [
   { url: "", title: "Home" },
   { url: "resume/", title: "Resume" }, 
   { url: "contact/", title: "Contact" },
   { url: "projects/", title: "Projects" }, 
-  { url: "https://github.com/rohanraval2", title: "GitHub" } // external
+  { url: "https://github.com/rohanraval2", title: "GitHub" } 
 ];
 
-// Create nav and add to top of body
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
-// Loop through pages and add each as a link
 for (let p of pages) {
   let url = p.url;
 
-  // Prefix internal links with BASE_PATH
   url = !url.startsWith("http") ? BASE_PATH + url : url;
 
-  // Create <a> tag
   let a = document.createElement("a");
   a.href = url;
   a.textContent = p.title;
 
-  // Highlight current page
   a.classList.toggle(
     "current",
     a.host === location.host && a.pathname === location.pathname
   );
 
-  // Open external links in new tab
   a.toggleAttribute("target", a.host !== location.host);
 
-  // Add link to nav
   nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+      Theme:
+      <select id="theme-switch">
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+    `
+  );
+  
+  function setColorScheme(scheme) {
+    document.documentElement.style.setProperty('color-scheme', scheme);
+    localStorage.colorScheme = scheme;
+    document.querySelector('#theme-switch').value = scheme;
+  }
+  
+  if ("colorScheme" in localStorage) {
+    setColorScheme(localStorage.colorScheme);
+  }
+  
+  document.querySelector('#theme-switch').addEventListener('input', (event) => {
+    setColorScheme(event.target.value);
+  });
+  
