9960b17d5aabd156b500bc2ac2fbeeb58e7c10ed
rohanraval2 <70520403+rohanraval2@users.noreply.github.com>
2025-04-25 20:26:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from 'portfolio/global.js';
 
-const allProjects = await fetchJSON('./lib/projects.json');
+const allProjects = await fetchJSON('portfolio/lib/projects.json');
 const latestProjects = allProjects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
@@ -8,23 +8,22 @@ renderProjects(latestProjects, projectsContainer, 'h2');
 
 const githubData = await fetchGitHubData('rohanraval2');
 const profileStats = document.querySelector('#profile-stats');
-if (profileStats) {
-    profileStats.innerHTML = `
-    <div class="stat">
-      <span class="label">Followers</span>
-      <span class="value">${githubData.followers}</span>
-    </div>
-    <div class="stat">
-      <span class="label">Following</span>
-      <span class="value">${githubData.following}</span>
-    </div>
-    <div class="stat">
-      <span class="label">Public Repos</span>
-      <span class="value">${githubData.public_repos}</span>
-    </div>
-    <div class="stat">
-      <span class="label">Public Gists</span>
-      <span class="value">${githubData.public_gists}</span>
-    </div>
-  `;
-}
+
+profileStats.innerHTML = `
+<div class="stat">
+    <span class="label">Followers</span>
+    <span class="value">${githubData.followers}</span>
+</div>
+<div class="stat">
+    <span class="label">Following</span>
+    <span class="value">${githubData.following}</span>
+</div>
+<div class="stat">
+    <span class="label">Public Repos</span>
+    <span class="value">${githubData.public_repos}</span>
+</div>
+<div class="stat">
+    <span class="label">Public Gists</span>
+    <span class="value">${githubData.public_gists}</span>
+</div>
+`;
